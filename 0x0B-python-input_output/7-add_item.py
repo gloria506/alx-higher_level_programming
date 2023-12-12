35c405464358f7af401b1962c4f1dff80f65fe4f
gloria506 <gloriaboateng2011@gmail.com>
2023-12-12 12:40:28 -0800

    input output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""a script that adds all arguments to a Python list
-and then save them to a file
-"""
+"""This module adds all arguments to a Python list and save them to a file."""
 
 
 import sys
