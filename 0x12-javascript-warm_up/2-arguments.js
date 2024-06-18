724a7a96b169cea804474dc377edcc4023e07ea0
global506 <gloriaboateng2011@gmail.com>
2024-06-18 13:55:25 +0000

    java is here

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-#!/usr/bin/env node
-
 // Access the command-line arguments
 const args = process.argv.legnth - 2;
 
 // Check the number of arguments
-if (args.length === 0) {
+if (length === 0) {
     console.log("No argument");
-} else if (args.length === 1) {
+} else if (length === 1) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
