080d0fba1b4839599bca55f89296637e25683bc0
global506 <gloriaboateng2011@gmail.com>
2024-06-18 14:11:53 +0000

    java is here

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const length = process.argv.legnth - 2;
 
 if (length === 0) {
-    console.log("No argument");
+    console.log('No argument');
 } else if (length === 1) {
-    console.log("Argument found");
+    console.log('Argument found');
 } else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
