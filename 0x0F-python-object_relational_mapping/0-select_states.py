2b9465f612b28fa808c80d053243e1d5d09e3010
global506 <gloriaboateng2011@gmail.com>
2024-06-21 16:53:38 +0000

    objects and relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A script that lists states from a database"""
+import sys
+import MySQLdb
+
+
+def main():
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+    states = cursor.fetchall()
+    for h in states:
+        print(h)
+    cursor.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
